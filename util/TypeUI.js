f227366733247c2c786e29e89367b0f4bec5c66a
MaiDTTM <68932625+MaiDao99@users.noreply.github.com>
2021-07-14 21:36:58 +0700

    API server client

--- util/TypeUI.js
+++ util/TypeUI.js
@@ -47,6 +47,7 @@ const url_api = {
     OPEN: '/open',
 };
 // const url_base = 'http://localhost:2020/api';
-const url_base = 'https://e90502fe5484.ngrok.io/api';
+// const url_base = 'https://e90502fe5484.ngrok.io/api';
+const url_base = 'http://192.168.20.103:2020/api';
 const url_base_img = `${url_base}/file/`;
 export { arrTypeUser, typeStore, url_api, url_base_img, url_base };
